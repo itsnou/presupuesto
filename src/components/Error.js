55300f8f368764a890a4da82a020ebf1a941ee14
itsnou <emiliano.meneses@gmail.com>
2020-11-01 16:17:52 -0300

    presupuesto

---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React from 'reat';
+
+const Error = () =>{
+    <p className="alert alert-danger error">Hubo un error</p>
+}
+
+export default Error;
---
+++ src/components/Pregunta.js
@@ -0,0 +1,52 @@
+import React, { Fragment,useState } from 'react';
+import Error from './Error';
+
+const Pregunta = () =>{
+
+    // definir state
+    const [cantidad, setCantidad] = useState(0);
+    const [error, setError] = useState(false);
+
+    // Leer el presupuesto
+    const definirPresupuesto = event =>{
+        setCantidad(parseInt(event.target.value))
+    }
+
+    // Submit para definitr el presupuesto
+    const agregarPresupuesto = add =>{
+        add.preventDefault();
+
+        // Validacion
+        if(cantidad<1 || isNaN(cantidad)){
+            setError(true)
+            return;
+        }
+
+        //Si pasa la validación
+        setError(false);
+
+    }
+
+    return(
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            {error? <Error/>: null}
+
+            <form onSubmit={agregarPresupuesto}>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+                <input 
+                type="submit"
+                className="button-primary u-full-width"
+                value="Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+    )
+}
+
+export default Pregunta;
